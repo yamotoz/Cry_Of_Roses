c3ba9ebbf151550183844014031a37d130584525
Miguel Oliveira <142369700+yamotoz@users.noreply.github.com>
2024-03-11 00:54:41 -0300

    Update Cry_of_Roses.sh

--- Cry_of_Roses.sh
+++ Cry_of_Roses.sh
@@ -12,7 +12,7 @@ echo ".";
 sleep 0.5;
 echo ".";
 sleep 0.5;
-echo "criando as pastas vuln, anonimato, domain, ozint, fish, wordlist"
+echo "criando as pastas vuln, anonimato, domain, ozint, fish, wordlist";
 mkdir /root/vuln anonimato domain ozint phish wordlist fuzz;
 echo ".";
 sleep 0.5;
@@ -109,11 +109,11 @@ sleep 0.5;
 echo "~";
 sleep 0.5;
 cd /root/ozint;
-echo "██████████████████████████████"
-echo "█─▄▄─█░▄▄░▄█▄─▄█▄─▀█▄─▄█─▄─▄─█"
-echo "█─██─██▀▄█▀██─███─█▄▀─████─███"
-echo "▀▄▄▄▄▀▄▄▄▄▄▀▄▄▄▀▄▄▄▀▀▄▄▀▀▄▄▄▀▀"
-git clone https://github.com/KasRoudra/PyPhisher && git clone https://github.com/jaykali/maskphish && git clone https://github.com/thewhiteh4t/seeker && git clone  https://github.com/Lucksi/Mr.Holmes && git clone https://github.com/exiftool/exiftool && https://github.com/techchipnet/CamPhish;
+echo "██████████████████████████████";
+echo "█─▄▄─█░▄▄░▄█▄─▄█▄─▀█▄─▄█─▄─▄─█";
+echo "█─██─██▀▄█▀██─███─█▄▀─████─███";
+echo "▀▄▄▄▄▀▄▄▄▄▄▀▄▄▄▀▄▄▄▀▀▄▄▀▀▄▄▄▀▀";
+git clone https://github.com/KasRoudra/PyPhisher && git clone https://github.com/jaykali/maskphish && git clone https://github.com/thewhiteh4t/seeker && git clone  https://github.com/Lucksi/Mr.Holmes && git clone https://github.com/exiftool/exiftool && git clone https://github.com/techchipnet/CamPhish;
 sleep 2;
 echo "█▀▀ █ █▄░█ █ █▀ █░█";
 echo "█▀░ █ █░▀█ █ ▄█ █▀█";
@@ -140,9 +140,9 @@ sleep 0.5;
 echo "~";
 sleep 0.5;
 cd /root/fuzz;
-echo"█▀▀ █──█ ▀▀█ ▀▀█" 
-echo"█▀▀ █──█ ▄▀─ ▄▀─"
-echo"▀── ─▀▀▀ ▀▀▀ ▀▀▀"
+echo"█▀▀ █──█ ▀▀█ ▀▀█" ;
+echo"█▀▀ █──█ ▄▀─ ▄▀─";
+echo"▀── ─▀▀▀ ▀▀▀ ▀▀▀";
 git clone https://github.com/devanshbatham/ParamSpider && git clone https://github.com/xmendez/wfuzz && cd wfuzz && pip install wfuzz && mkdir wordFUZZ && cd wordFUZZ && wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Fuzzing/LFI/LFI-gracefulsecurity-windows.txt && wget https://github.com/danielmiessler/SecLists/blob/master/Fuzzing/LFI/LFI-etc-files-of-all-linux-packages.txt && wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Fuzzing/403/403.md;
 sleep 2;
 echo "█▀▀ █ █▄░█ █ █▀ █░█";
@@ -155,8 +155,8 @@ sleep 0.5;
 echo "~";
 sleep 0.5;
 cd /root/wordlist;
-echo "█░█░█ █▀█ █▀█ █▀▄ █░░ █ █▀ ▀█▀"
-echo "▀▄▀▄▀ █▄█ █▀▄ █▄▀ █▄▄ █ ▄█ ░█░"
+echo "█░█░█ █▀█ █▀█ █▀▄ █░░ █ █▀ ▀█▀";
+echo "▀▄▀▄▀ █▄█ █▀▄ █▄▀ █▄▄ █ ▄█ ░█░";
 mkdir gf && cd gf && wget https://raw.githubusercontent.com/1ndianl33t/Gf-Patterns/master/sqli.json && wget https://raw.githubusercontent.com/1ndianl33t/Gf-Patterns/master/lfi.json && wget https://github.com/1ndianl33t/Gf-Patterns/blob/master/ssrf.json && wget https://raw.githubusercontent.com/1ndianl33t/Gf-Patterns/master/redirect.json && wget https://raw.githubusercontent.com/1ndianl33t/Gf-Patterns/master/interestingsubs.json && wget https://raw.githubusercontent.com/1ndianl33t/Gf-Patterns/master/idor.json && wget https://raw.githubusercontent.com/1ndianl33t/Gf-Patterns/master/xss.json && cd /root/wordlist && mkdir dirb && cd dirb && wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/URLs/urls-wordpress-3.3.1.txt && wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/directory-list-2.3-big.txt && cd /root/wordlist; gzip -d /usr/share/wordlists/rockyou.txt.gz; cp /usr/share/wordlists/rockyou.txt /root/wordlist;
 sleep 2;
 echo "█▀▀ █ █▄░█ █ █▀ █░█";
@@ -169,12 +169,12 @@ sleep 0.5;
 echo "~"
 sleep 0.5;
 apt-get autoremove -y;
-echo -e "\e[32m██╗░░██╗░█████╗░░█████╗░██╗░░██╗  ████████╗██╗███╗░░░███╗███████╗  ███╗░░██╗░█████╗░░██╗░░░░░░░██╗"
-echo "██║░░██║██╔══██╗██╔══██╗██║░██╔╝  ╚══██╔══╝██║████╗░████║██╔════╝  ████╗░██║██╔══██╗░██║░░██╗░░██║"
-echo "███████║███████║██║░░╚═╝█████═╝░  ░░░██║░░░██║██╔████╔██║█████╗░░  ██╔██╗██║██║░░██║░╚██╗████╗██╔╝"
-echo "██╔══██║██╔══██║██║░░██╗██╔═██╗░  ░░░██║░░░██║██║╚██╔╝██║██╔══╝░░  ██║╚████║██║░░██║░░████╔═████║░"
-echo "██║░░██║██║░░██║╚█████╔╝██║░╚██╗  ░░░██║░░░██║██║░╚═╝░██║███████╗  ██║░╚███║╚█████╔╝░░╚██╔╝░╚██╔╝░"
-echo "╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝  ░░░╚═╝░░░╚═╝╚═╝░░░░░╚═╝╚══════╝  ╚═╝░░╚══╝░╚════╝░░░░╚═╝░░░╚═╝░░\e[0m"
+echo -e "\e[32m██╗░░██╗░█████╗░░█████╗░██╗░░██╗  ████████╗██╗███╗░░░███╗███████╗  ███╗░░██╗░█████╗░░██╗░░░░░░░██╗";
+echo "██║░░██║██╔══██╗██╔══██╗██║░██╔╝  ╚══██╔══╝██║████╗░████║██╔════╝  ████╗░██║██╔══██╗░██║░░██╗░░██║";
+echo "███████║███████║██║░░╚═╝█████═╝░  ░░░██║░░░██║██╔████╔██║█████╗░░  ██╔██╗██║██║░░██║░╚██╗████╗██╔╝";
+echo "██╔══██║██╔══██║██║░░██╗██╔═██╗░  ░░░██║░░░██║██║╚██╔╝██║██╔══╝░░  ██║╚████║██║░░██║░░████╔═████║░";
+echo "██║░░██║██║░░██║╚█████╔╝██║░╚██╗  ░░░██║░░░██║██║░╚═╝░██║███████╗  ██║░╚███║╚█████╔╝░░╚██╔╝░╚██╔╝░";
+echo "╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝  ░░░╚═╝░░░╚═╝╚═╝░░░░░╚═╝╚══════╝  ╚═╝░░╚══╝░╚════╝░░░░╚═╝░░░╚═╝░░";
 
 
 
